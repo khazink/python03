1c950a6baac9ce9922f0b92862bcea97ec642350
khazink <khairellkhazin@gmail.com>
2026-03-17 01:49:53 +0800

    python03:ex2

--- ex2/ft_coordinate_system.py
+++ ex2/ft_coordinate_system.py
@@ -2,16 +2,12 @@ import math
 
 
 def parse_position(position: tuple) -> tuple:
-    for i in position:
         try:
-            int(i)
-        except ValueError:
-            print(f"Error parsing coordinate: invalid literal for int() with "
-                  f"base 10: {i}")
-            print("Error details - Type: ValueError, Args(\"invalid literal "
-                  f"for int() with base 10: '{i}'\",)")
-            return
-    return(position)
+            return tuple(int(x) for x in position)
+        except ValueError as e:
+            print(f"Error parsing coordinate: {e}")
+            print(f"Error details - Type: ValueError, Args: {e.args}")
+            raise ValueError
 
 
 def calculate_distance(position1: tuple, position2: tuple) -> float:
@@ -22,13 +18,26 @@ def calculate_distance(position1: tuple, position2: tuple) -> float:
 
 def main() -> None:
     pos_create = (10, 20, 5)
-    position = parse_position(pos_create)
-    if position is None:
-        return
-    print(f"Position created: {pos_create}")
+    try:
+         position = parse_position(pos_create)
+    except ValueError:
+         return
+    else:
+        print(f"Position created: {position}")
     pos_origin = (0, 0, 0)
     distance = calculate_distance(pos_create, pos_origin)
     print(f"Distance between {pos_origin} and {pos_create}: {distance:.2f}")
+    parse_coordinate = (3,4,0)
+    print()
+    print(f"Parsing coordinates: {parse_coordinate}")
+    try:
+         position = parse_position(parse_coordinate)
+    except ValueError:
+         return
+    else:
+         print(f"Parsed position: {position}")
+    distance = calculate_distance(parse_coordinate, pos_origin)
+    print(f"Distance between {pos_origin} and {parse_coordinate}: {distance:.2f}")
 
 
 if __name__ == "__main__":
